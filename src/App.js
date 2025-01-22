1cfeefc35bdf8501e1fa3b85ce25e79108b9e65e
Yash Vardhan <yash@Yashs-MacBook-Air.local>
2025-01-22 23:35:17 +0530

    Made some basic layout changes to the website

--- src/App.js
+++ src/App.js
@@ -1,5 +1,40 @@
 import React from "react";
+import "./App.css";
 
 export default function App() {
-  return <div></div>;
+  return (
+    <div className="main">
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <div className="header_container">
+      <h1>JAGA</h1>
+      <div>{/* Two SVG for the LinkedIn and GitHub */}</div>
+      <div>{/* Dropdown menu */}</div>
+    </div>
+  );
+}
+
+function Body() {
+  return (
+    <div className="body_container">
+      {/* Work Experience */}
+      {/* Skills */}
+      {/* Contacts */}
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <div className="footer_container">
+      <h4>Copywrite 2025 Jaga</h4>
+    </div>
+  );
 }
